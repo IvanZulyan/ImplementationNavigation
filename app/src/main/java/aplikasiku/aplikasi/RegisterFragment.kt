887ed07dfc6842d8952965da28cca28088f8b592
Ivan Zulyan <zulyanivan11@gmail.com>
2022-09-07 18:32:20 +0700

    Initial commit

---
+++ app/src/main/java/aplikasiku/aplikasi/RegisterFragment.kt
@@ -0,0 +1,33 @@
+package aplikasiku.aplikasi
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import aplikasiku.aplikasi.databinding.FragmentRegisterBinding
+
+
+class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRegisterBinding.inflate(inflater,container,false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnregister.setOnClickListener {
+            view.findNavController().navigate(R.id.action_registerFragment_to_homeFragment)
+        }
+
+    }
+}
